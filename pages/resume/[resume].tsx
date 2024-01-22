7cb9b30b347312c7dba645d9cef9661ec154b570
Om <ommukherjee28@gmail.com>
2024-01-22 13:54:00 +0530

    fix:ui

--- pages/resume/[resume].tsx
+++ pages/resume/[resume].tsx
@@ -1,6 +1,24 @@
-
 import Resume from "@/components/Resume";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
+
+export async function getServerSideProps(context: NextPageContext){
+    const session = await getSession(context);
+  
+    if(!session){
+      return {
+        redirect:{
+          destination:'/auth',
+          permanent:false,
+        }
+      }
+    }
+  
+    return {
+      props:{}
+    }
+}
 
 const ResumePage = () => {
     const router = useRouter();
