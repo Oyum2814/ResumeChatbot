aed0251e75d2ea103739476327adbd78deacbdff
Om <ommukherjee28@gmail.com>
2024-02-17 00:49:27 +0530

    fix:ui resume page

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -46,7 +46,7 @@ const Breadcrumbs:React.FC<BreadcrumbsProps> = ({template,resume,resumeName}) =>
                         <svg className="rtl:rotate-180 w-3 h-3  mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
                         </svg>
-                        <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:">{resumeName}</span>
+                        <span className="ms-1 text-sm font-medium text-white md:ms-2 dark:">{resumeName}</span>
                         </div>
                     </li>
                 </>
